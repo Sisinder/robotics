c36254ee1d44a3e6b40aa886b7494a2b6fcb3f8d
Sisinder <44865474+Sisinder@users.noreply.github.com>
2020-02-20 00:25:46 +0530

    update

--- form.php
+++ form.php
@@ -1,89 +1,137 @@
 
+<?php
+//index.php
 
+$error = '';
+$name = '';
+$email = '';
+$subject = '';
+$message = '';
 
+function clean_text($string)
+{
+ $string = trim($string);
+ $string = stripslashes($string);
+ $string = htmlspecialchars($string);
+ return $string;
+}
 
+if(isset($_POST["submit"]))
+{
+ if(empty($_POST["name"]))
+ {
+  $error .= '<p><label class="text-danger">Please Enter your Name</label></p>';
+ }
+ else
+ {
+  $name = clean_text($_POST["name"]);
+  if(!preg_match("/^[a-zA-Z ]*$/",$name))
+  {
+   $error .= '<p><label class="text-danger">Only letters and white space allowed</label></p>';
+  }
+ }
+ if(empty($_POST["email"]))
+ {
+  $error .= '<p><label class="text-danger">Please Enter your Email</label></p>';
+ }
+ else
+ {
+  $email = clean_text($_POST["email"]);
+  if(!filter_var($email, FILTER_VALIDATE_EMAIL))
+  {
+   $error .= '<p><label class="text-danger">Invalid email format</label></p>';
+  }
+ }
+ if(empty($_POST["subject"]))
+ {
+  $error .= '<p><label class="text-danger">Subject is required</label></p>';
+ }
+ else
+ {
+  $subject = clean_text($_POST["subject"]);
+ }
+ if(empty($_POST["message"]))
+ {
+  $error .= '<p><label class="text-danger">Message is required</label></p>';
+ }
+ else
+ {
+  $message = clean_text($_POST["message"]);
+ }
+ if($error == '')
+ {
+  require 'class/class.phpmailer.php';
+  $mail = new PHPMailer;
+  $mail->IsSMTP();        //Sets Mailer to send message using SMTP
+  $mail->Host = 'smtpout.secureserver.net';  //Sets the SMTP hosts
+  $mail->Port = '80';        //Sets the default SMTP server port
+  $mail->SMTPAuth = true;       //Sets SMTP authentication. Utilizes the Username and Password variables
+  $mail->Username = 'developerwork';     //Sets SMTP username
+  $mail->Password = 'SIsinder';     //Sets SMTP password
+  $mail->SMTPSecure = '';       //Sets connection prefix. Options are "", "ssl" or "tls"
+  $mail->From = $_POST["email"];     //Sets the From email address for the message
+  $mail->FromName = $_POST["name"];    //Sets the From name of the message
+  $mail->AddAddress('info@find2rent.com', 'Name');//Adds a "To" address
+  $mail->AddCC($_POST["email"], $_POST["name"]); //Adds a "Cc" address
+  $mail->WordWrap = 50;       //Sets word wrapping on the body of the message to a given number of characters
+  $mail->IsHTML(true);       //Sets message type to HTML    
+  $mail->Subject = $_POST["subject"];    //Sets the Subject of the message
+  $mail->Body = $_POST["message"];    //An HTML or plain text message body
+  if($mail->Send())        //Send an Email. Return true on success or false on error
+  {
+   $error = '<label class="text-success">Thank you for contacting us</label>';
+  }
+  else
+  {
+   $error = '<label class="text-danger">There is an Error</label>';
+  }
+  $name = '';
+  $email = '';
+  $subject = '';
+  $message = '';
+ }
+}
 
-<section id="contact" style="background: #efefef;padding-top:0px;padding-bottom:20px;color: #777;">
-			<div class="container" style="padding-top:120px">
-				<h1>GET IN<span style="color:#00bfff"> TOUCH</span></h1>
-				<div class="row">
-					<div class="col-md-6 contact-info" >
-						<form action="userinfo.php" method="post">
-					<div class="form-group">
-						<label>UserName</label>
-						<input type="text" name="username" autocomplete="off" class="form-control">
-
-					</div>
-					<div class="form-group">
-						<label>Email ID</label>
-						<input type="text" name="email" autocomplete="off" class="form-control">
-
-					</div>
-					<div class="form-group">
-						<label>Mobile</label>
-						<input type="text" name="mobile" autocomplete="off" class="form-control">
-
-					</div>
-					<div class="form-group">
-						<label>comments</label>
-						<textarea class="form-control" name="comment">
-							
-						</textarea>
-
-					</div>
-					<button type="submit" class="btn btn-primary">Submit</button>
-				</form>						<div class="col-md-20" style="padding-top:40px">
-															<div class="row">
-																<p style="word-spacing:30px"> <a href="https://www.facebook.com/soc.softech" class="fa fa-facebook" style="font-size:30px"></a>
-																	<a href="https://twitter.com/SocSoftech" class="fa fa-twitter" style="font-size:30px"></a>
-																	<a href="https://www.linkedin.com/in/soc-softech-716473152/" class="fa fa-linkedin" style="font-size:30px"></a>
-																	<a href="https://www.youtube.com/socsoftech" class="fa fa-youtube" style="font-size:30px"></a>
-																	<a href="https://www.instagram.com/socsoftech/" class="fa fa-instagram" style="font-size:30px"></a>
-																</p>
-															</div>
-							</div>
-					</div>
-			
-        <!-- Start Main Content Wrapper -->
-
-					<div class="col-md-6 contact-info" >
-						<section class="intro">
-							<div style="padding-top:0px !important; padding-left:70px;" class="container">
-								<div class="section-content" >
-									<h4><i class="fa fa-home"></i>&nbsp;&nbsp;<strong style="color: #18548b"> ROBOTICS RESEARCH CENTER</strong></h4><br>
-                <!-- Paragraph -->
-									<ul class="contact-info" style="list-style: none">
-										<li style="color: #3f3a36;line-height: 30px"><i class="fa fa-map-marker" aria-hidden="true"></i><b>ADDRESS:</b>
-										<p style="padding-left:0px;">#102, Star Meena Mehatab Nivas ,
-										<br>Beside Satyam Theatre, Opp: Gurudwara,
-										<br>Ameerpet, Hyderabad,Telangana,500038.</p> </li>
-										<li style="color: #3f3a36;line-height: 30px"><i class="fa fa-phone" aria-hidden="true"></i><b>Phone number:</b><p style="padding-left:0px;">       </p></li>
-                                       
-										<li><i class="fa fa-envelope" style="color: #000"></i>&nbsp;&nbsp;&nbsp;<b style="line-height: 30px;color: #000">Email-id: </b></li>
-                    
-									</ul>
-									
-								</div>
-							</div>
-						</section><br>
-					</div>
-					
-				</div>
-				<!------end of row --->
-			</div>
-		</section>
-		
-	<div style="background-color:black">
-					<footer class="page-footer font-small blue">
-
-  <!-- Copyright -->
-						<div class="footer-copyright text-center py-3" style="color:#C6C4C4"> © 2019 Copyright:
-							<a href="index.html"><span style="color:#00bfff"> SocSoftech.com.</span></a>
-								<span > All Rights Reserved.</span>
-						</div>
-  <!-- Copyright -->
-
-					</footer>
-				</div>
-<!-- Footer -->
-
+?>
+<!DOCTYPE html>
+<html>
+ <head>
+  <title>Send an Email on Form Submission using PHP with PHPMailer</title>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+ </head>
+ <body>
+  <br />
+  <div class="container">
+   <div class="row">
+    <div class="col-md-8" style="margin:0 auto; float:none;">
+     <h3 align="center">Send an Email on Form Submission using PHP with PHPMailer</h3>
+     <br />
+     <?php echo $error; ?>
+     <form method="post">
+      <div class="form-group">
+       <label>Enter Name</label>
+       <input type="text" name="name" placeholder="Enter Name" class="form-control" value="<?php echo $name; ?>" />
+      </div>
+      <div class="form-group">
+       <label>Enter Email</label>
+       <input type="text" name="email" class="form-control" placeholder="Enter Email" value="<?php echo $email; ?>" />
+      </div>
+      <div class="form-group">
+       <label>Enter Subject</label>
+       <input type="text" name="subject" class="form-control" placeholder="Enter Subject" value="<?php echo $subject; ?>" />
+      </div>
+      <div class="form-group">
+       <label>Enter Message</label>
+       <textarea name="message" class="form-control" placeholder="Enter Message"><?php echo $message; ?></textarea>
+      </div>
+      <div class="form-group" align="center">
+       <input type="submit" name="submit" value="Submit" class="btn btn-info" />
+      </div>
+     </form>
+    </div>
+   </div>
+  </div>
+ </body>
+</html>
